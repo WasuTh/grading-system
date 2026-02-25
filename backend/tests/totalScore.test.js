afd0df8a030888e4eaa22d12fdadb4f9c2e8616b
WasuTh <146047771+WasuTh@users.noreply.github.com>
2026-02-25 20:26:39 +0700

    refactor: rename calculateWeightedScore to calculateTotalScore

---
+++ backend/tests/totalScore.test.js
@@ -0,0 +1,29 @@
+import { calculateTotalScore } from "../src/services/totalScore.serverice.js";
+import { course, studentScore } from "../src/data/mock.js";
+
+const total = calculateTotalScore(
+    studentScore[0].score,
+    course.gradingPolicy
+);
+const total1 = calculateTotalScore(
+    studentScore[1].score,
+    course.gradingPolicy
+);
+const total2 = calculateTotalScore(
+    studentScore[2].score,
+    course.gradingPolicy
+);
+const total3 = calculateTotalScore(
+    studentScore[3].score,
+    course.gradingPolicy
+);
+const total4 = calculateTotalScore(
+    studentScore[4].score,
+    course.gradingPolicy
+);
+
+console.log(total);
+console.log(total1);
+console.log(total2);
+console.log(total3);
+console.log(total4);
