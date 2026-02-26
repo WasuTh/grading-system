d5a77222ebb6a89ddb6996c6f0493513fc98ed3d
WasuTh <146047771+WasuTh@users.noreply.github.com>
2026-02-26 15:29:27 +0700

    feat(grading): clean up and rewrite grading

--- backend/src/services/grading.service.js
+++ backend/src/services/grading.service.js
@@ -1,10 +1,36 @@
-import { calculateTotalScore } from "../utils/totalScore.js";
+import { calculateTotalScore } from "./totalScore.service.js"
+import {
+    calculateMean,
+    calculateStdDev,
+    calculatePercentile,
+    calculateTScore
+} from "./statistics.service.js";
 
-export function validateWeight(weights) {
-    const total = weights.coursework + weights.midterm + weights.final;
-    return total === 100;
-}
+export function generateGradeReport(course, studentScore) {
 
-export function calculateStudentTotal(data) {
-    return calculateTotalScore(data);
+    // คำนวณ totalScore
+    const policy = course.gradingPolicy;
+    const stuTotal = studentScore.map((stuScore) => ({
+        no: stuScore.no,
+        totalScore: calculateTotalScore(stuScore.score, policy)
+    }));
+
+    // คำนวณสถิติรวม
+    const totalScores = stuTotal.map(s => s.totalScore);
+    const mean = calculateMean(totalScores);
+    const stdDev = calculateStdDev(totalScores);
+
+    // รวมผลลัพธ์
+    return stuTotal.map(student => {
+        const percentile = calculatePercentile(totalScores, student.totalScore)
+        const tScore = calculateTScore(student.totalScore, mean, stdDev)
+
+        const result = {
+            no: student.no,
+            totalScore: Number(student.totalScore.toFixed(2)),
+            percentile: Number(percentile.toFixed(2)),
+            tScore: Number(tScore.toFixed(2))
+        }
+        return result;
+    })
 }
